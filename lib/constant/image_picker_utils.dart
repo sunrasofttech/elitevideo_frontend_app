ed060c28daf668893511733277a9dfacb4897453
sagar-81 <sagarkhemnar143@gmail.com>
2026-01-09 14:36:14 +0530

    changes

--- lib/constant/image_picker_utils.dart
+++ lib/constant/image_picker_utils.dart
@@ -22,30 +22,4 @@ class ImagePickerUtil {
     final pickedFile = await _picker.pickVideo(source: ImageSource.camera);
     return pickedFile;
   }
-
-  //  static Future<XFile?> _cropImage(XFile? pickedFile, CropAspectRatio? aspectRatio) async {
-  //   if (pickedFile == null) return null;
-
-  //   final croppedFile = await ImageCropper().cropImage(
-  //     sourcePath: pickedFile.path,
-  //     // aspectRatio: aspectRatio ?? const CropAspectRatio(ratioX: 16, ratioY: 9),
-  //     compressFormat: ImageCompressFormat.jpg,
-  //     compressQuality: 85,
-  //     uiSettings: [
-  //       AndroidUiSettings(
-  //         toolbarTitle: 'Crop Image',
-  //         toolbarColor: Colors.black,
-  //         toolbarWidgetColor: Colors.white,
-  //         lockAspectRatio: false,
-  //         initAspectRatio: CropAspectRatioPreset.original,
-  //       ),
-  //       IOSUiSettings(
-  //         title: 'Crop Image',
-  //         aspectRatioLockEnabled: true,
-  //       ),
-  //     ],
-  //   );
-
-  //   return croppedFile != null ? XFile(croppedFile.path) : null;
-  // }
 }
